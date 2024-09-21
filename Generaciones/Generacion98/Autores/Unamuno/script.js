604657781e6f3ee6b645f280ad71008b60eaf4e0
ManuFlosoYT <manubh94y@gmail.com>
2024-09-21 19:22:35 +0200

    Poesia Social

--- Generaciones/Generacion98/Autores/Unamuno/script.js
+++ Generaciones/Generacion98/Autores/Unamuno/script.js
@@ -70,13 +70,19 @@ fetch('Textos/castilla.txt')
 
 //Rosario de sonetos líricos
 
-fetch('Textos/rosario.txt')
-    .then(response => response.text())
-    .then(text => 
-    {
-        document.getElementById('rosario').textContent = text;
-    }
-).catch(error => console.error('Error fetching file:', error));
+fetch('Textos/biografia2.txt') //CAMBIAR ESTO PARA CONFIGURAR
+  .then(response => response.text())
+  .then(texto => {
+    const frasesEnNegrita = /(tendencia a la vanguardia|tendencia al compromiso)/gi; //CAMBIAR ESTO PARA CONFIGURAR
+    const frasesEnCursiva = /(Soledades|Campos de Castilla|Nuevas canciones|La Lola se va a los puertos|La Lola se va a los puertos)/gi; //CAMBIAR ESTO PARA CONFIGURAR
+    const frasesSubrayadas = /(texto a subrayar|otro texto)/gi; //CAMBIAR ESTO PARA CONFIGURAR
+
+    const textoFormateado1 = texto.replace(frasesEnNegrita, '<b>$&</b>'); 
+    const textoFormateado2 = textoFormateado1.replace(frasesEnCursiva, '<i>$&</i>'); 
+    const textoFinal = textoFormateado2.replace(frasesSubrayadas, '<u>$&</u>');
+
+    document.getElementById('biografia2').innerHTML = textoFinal; //CAMBIAR ESTO PARA CONFIGURAR
+}).catch(error => console.error('Error al cargar el archivo:', error));;
 
 fetch('Textos/rosario2.txt')
     .then(response => response.text())
